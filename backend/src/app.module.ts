ffd9c6c065f4697951f4b57fcc8226d885eb2625
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-21 19:56:36 +0800

    refactor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports: [RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
