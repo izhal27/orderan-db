4e36fd45277764318741e4cffcd50887994a2b35
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-23 11:11:45 +0800

    add validator decorators

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,51 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  password: string;
+
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(50)
+  @ApiProperty({ required: false, nullable: true })
+  name: string | null;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @ApiProperty({ required: false, nullable: true })
+  image: string | null;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false, nullable: true })
+  @ApiProperty()
+  isBlocked: boolean;
+
+  @IsBoolean()
+  @ApiProperty({ required: false, nullable: true })
+  @ApiProperty()
+  roleId: number;
+
+  @ApiProperty({ required: false, nullable: true })
+  refreshToken: string | null;
+
+}
