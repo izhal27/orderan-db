f9effb7aefedb6cdf68e5862bc09902bbeef0a1e
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-23 10:55:55 +0800

    add api property decorators

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  name: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  image: string | null;
+
+  @ApiProperty()
+  isBlocked: boolean;
+
+  @ApiProperty()
+  roleId: number;
+
+  @ApiProperty({ required: false, nullable: true })
+  refreshToken: string | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
