92e6f23caa9c072b7e0dd63b36e4188d684cb10c
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-23 14:22:22 +0800

    add global role type

--- backend/src/roles/roles.controller.spec.ts
+++ backend/src/roles/roles.controller.spec.ts
@@ -1,11 +1,19 @@
-import { BadRequestException, ConflictException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException, ConflictException, NotFoundException } from '@nestjs/common';
 import { Role } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
 
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+
+const role: Role = {
+  id: 1,
+  name: 'New role',
+  description: 'New description',
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
 
 describe('RolesController', () => {
   let rolesController: RolesController;
@@ -28,40 +36,22 @@ describe('RolesController', () => {
   describe('roles controller', () => {
     describe('create role', () => {
       it('should register new role', async () => {
-        const newRole = {
-          name: 'New role',
-          description: 'New description'
-        };
-
-        const mockRegisterResponse: Role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        };
-
         jest
           .spyOn(rolesService, 'create')
-          .mockResolvedValue(mockRegisterResponse);
+          .mockResolvedValue(role);
 
-        const result = await rolesController.create(newRole);
+        const result = await rolesController.create(role);
 
-        expect(rolesService.create).toHaveBeenCalledWith(newRole);
-        expect(result).toEqual(mockRegisterResponse);
+        expect(rolesService.create).toHaveBeenCalledWith(role);
+        expect(result).toEqual(role);
       });
 
       it('should throw error if name already registered', async () => {
-        const registeredUser = {
-          name: 'New role',
-          description: 'New description'
-        };
-
         jest
           .spyOn(rolesService, 'create')
           .mockRejectedValue(new ConflictException());
 
-        const register = rolesController.create(registeredUser);
+        const register = rolesController.create(role);
 
         await expect(rolesService.create).rejects.toThrow(ConflictException);
         await expect(register).rejects.toThrow(ConflictException);
@@ -81,21 +71,13 @@ describe('RolesController', () => {
 
     describe('get role', () => {
       it('should return a role', async () => {
-        const role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        };
-
         jest
           .spyOn(rolesService, 'findOne')
           .mockResolvedValue(role);
 
-        const result = await rolesController.findOne(1);
+        const result = await rolesController.findOne(role.id);
 
-        expect(rolesService.findOne).toHaveBeenCalledWith(1);
+        expect(rolesService.findOne).toHaveBeenCalledWith(role.id);
         expect(result).toEqual(role);
       });
 
@@ -104,7 +86,7 @@ describe('RolesController', () => {
           .spyOn(rolesService, 'findOne')
           .mockRejectedValue(new NotFoundException());
 
-        const result = rolesController.findOne(1);
+        const result = rolesController.findOne(role.id);
 
         await expect(rolesService.findOne).rejects.toThrow(NotFoundException);
         await expect(result).rejects.toThrow(NotFoundException);
@@ -113,12 +95,6 @@ describe('RolesController', () => {
 
     describe('update role', () => {
       it('should throw error if role not found', async () => {
-        const role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-        };
-
         jest
           .spyOn(rolesService, 'update')
           .mockRejectedValue(new NotFoundException());
@@ -130,28 +106,14 @@ describe('RolesController', () => {
       });
 
       it('should update role', async () => {
-        const role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-        };
-
-        const mockUpdateResponse: Role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        };
-
         jest
           .spyOn(rolesService, 'update')
-          .mockResolvedValue(mockUpdateResponse);
+          .mockResolvedValue(role);
 
         const result = await rolesController.update(role.id, role);
 
         expect(rolesService.update).toHaveBeenCalledWith(role.id, role);
-        expect(result).toEqual(mockUpdateResponse);
+        expect(result).toEqual(role);
       });
     });
 
@@ -161,21 +123,13 @@ describe('RolesController', () => {
           .spyOn(rolesService, 'remove')
           .mockRejectedValue(new NotFoundException());
 
-        const result = rolesController.remove(1);
+        const result = rolesController.remove(role.id);
 
         await expect(rolesService.remove).rejects.toThrow(NotFoundException);
         await expect(result).rejects.toThrow(NotFoundException);
       });
 
       it('should return a role when success deleted', async () => {
-        const role = {
-          id: 1,
-          name: 'New role',
-          description: 'New description',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        };
-
         jest
           .spyOn(rolesService, 'remove')
           .mockResolvedValue(role);
