a80781cefd42fb9a07d3710edb5dedc0e7c6da1f
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-28 09:05:41 +0800

    refactor access_token

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -11,79 +11,86 @@ import { RolesModule } from '../src/roles/roles.module';
 describe('Auth (e2e)', () => {
   let app: INestApplication;
   let prismaClient: PrismaClient;
+  let accessToken;
+  const user = {
+    username: 'user1',
+    password: 'aaa',
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule, RolesModule, PrismaClient],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
     await app.init();
-
     prismaClient = moduleFixture.get<PrismaClient>(PrismaClient);
     await prismaClient.role.upsert({
       where: { name: 'Role-1' },
       update: {},
       create: { name: 'Role-1', description: 'Description Role-1' },
     });
-
     await prismaClient.$executeRaw`TRUNCATE "public"."User" RESTART IDENTITY CASCADE;`;
   }, 30000);
 
   afterAll(async () => {
+    await prismaClient.$executeRaw`TRUNCATE "public"."User" RESTART IDENTITY CASCADE;`;
     await app.close();
     await prismaClient.$disconnect();
   }, 30000);
 
   describe('Unauthenticated', () => {
-    it('/roles (GET) - should return Unauthorized', () => {
-      return request(app.getHttpServer()).get('/roles').expect(401);
+    it('GET: /roles - should return Unauthorized', async () => {
+      await request(app.getHttpServer())
+        .get('/roles')
+        .expect(401);
     });
 
-    it('/users (GET) - should return Unauthorized', () => {
-      return request(app.getHttpServer()).get('/users').expect(401);
+    it('GET: /users - should return Unauthorized', async () => {
+      await request(app.getHttpServer())
+        .get('/users')
+        .expect(401);
     });
   });
 
   describe('Authenticated', () => {
-    const user = {
-      username: 'user1',
-      password: 'aaa',
-    };
-    let token;
-
-    it('/auth/local/signup (POST) - should return access_token and refresh_token', async () => {
+    it('POST: /auth/local/signup - should return tokens', async () => {
       const res = await request(app.getHttpServer())
         .post('/auth/local/signup')
         .send(user)
         .expect(201);
-      const data = await res.body;
-      expect(data.access_token).not.toBeNull();
-      expect(data.refresh_token).not.toBeNull();
+      const { access_token, refresh_token } = await res.body;
+      accessToken = access_token
+      expect(access_token).not.toBeNull();
+      expect(refresh_token).not.toBeNull();
     });
 
-    it('/roles (GET) - should return roles array', async () => {
-      const resLogin = await request(app.getHttpServer())
+    it('POST: /auth/local/signin - should return tokens', async () => {
+      const res = await request(app.getHttpServer())
         .post('/auth/local/signin')
         .send(user)
         .expect(200);
-      token = await resLogin.body?.access_token;
+      const { access_token, refresh_token } = await res.body;
+      expect(access_token).not.toBeNull();
+      expect(refresh_token).not.toBeNull();
+    });
+
+    it('GET: /roles - should return roles array', async () => {
       const res = await request(app.getHttpServer())
         .get('/roles')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
       const roles = await res.body;
       expect(roles).not.toBeNull();
       expect(roles).toBeInstanceOf(Array);
     });
 
-    it('/users (GET) - should return users array', async () => {
+    it('GET: /users - should return users array', async () => {
       const res = await request(app.getHttpServer())
         .get('/users')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
       const users = await res.body;
       expect(users).not.toBeNull();
