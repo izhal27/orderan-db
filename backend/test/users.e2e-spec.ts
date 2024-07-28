338958261b3e9c0549fd35760949c5f5945a0842
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-28 23:10:11 +0800

    update users e2e testing

--- backend/test/users.e2e-spec.ts
+++ backend/test/users.e2e-spec.ts
@@ -3,11 +3,17 @@ import * as request from 'supertest';
 
 import { PrismaClient } from '@prisma/client';
 import { buildApp } from './setup.e2e';
+import { compareValue } from 'src/helpers/hash';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
   let prismaClient: PrismaClient;
-  const user = {
+  let user: {
+    id: number | null;
+    username: string;
+    password: string;
+  } = {
+    id: null,
     username: 'user1',
     password: '12345',
   };
@@ -16,6 +22,11 @@ describe('UserController (e2e)', () => {
   beforeAll(async () => {
     ({ app, prismaClient } = await buildApp());
     await prismaClient.$executeRaw`TRUNCATE "public"."User" RESTART IDENTITY CASCADE;`;
+    await prismaClient.role.upsert({
+      where: { name: 'Role-1' },
+      update: {},
+      create: { name: 'Role-1', description: 'Description Role-1' },
+    });
     const res = await request(app.getHttpServer())
       .post('/auth/local/signup')
       .send(user)
@@ -25,6 +36,7 @@ describe('UserController (e2e)', () => {
 
   afterAll(async () => {
     await prismaClient.$executeRaw`TRUNCATE "public"."User" RESTART IDENTITY CASCADE;`;
+    await prismaClient.$executeRaw`TRUNCATE "public"."Role" RESTART IDENTITY CASCADE;`;
     await app.close();
     await prismaClient.$disconnect();
   });
@@ -35,6 +47,22 @@ describe('UserController (e2e)', () => {
         .post('/users')
         .set('Authorization', `Bearer ${accessToken}`)
         .send({ ...user, username: '' })
+        .expect(400);
+    });
+
+    it('should throw error 400 when password is missing', async () => {
+      await request(app.getHttpServer())
+        .post('/users')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ ...user, password: '' })
+        .expect(400);
+    });
+
+    it('should throw error 400 when email is wrong', async () => {
+      await request(app.getHttpServer())
+        .post('/users')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ ...user, email: 'testemail' })
         .expect(400);
     });
 
@@ -52,9 +80,90 @@ describe('UserController (e2e)', () => {
         .set('Authorization', `Bearer ${accessToken}`)
         .send({ username: 'user2', password: '12345' })
         .expect(201);
-      const newUser = await res.body;
-      expect(newUser).not.toBeNull();
-      expect(newUser.username).toEqual('user2');
+      user = await res.body;
+      expect(user).not.toBeNull();
+      expect(user.username).toEqual('user2');
+    });
+  });
+
+  describe('Read', () => {
+    it('POST: /users/a - should throw error 400 when param input wrong', async () => {
+      await request(app.getHttpServer())
+        .get('/users/a')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(400);
+    });
+
+    it('POST: /users/1000 - should throw error 404 when user not found', async () => {
+      await request(app.getHttpServer())
+        .get('/users/1000')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(404);
+    });
+
+    it('should return list of users', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/roles')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200);
+      const data = await res.body;
+      expect(data).not.toBe(null);
+      expect(data).toBeInstanceOf(Array);
+    });
+
+    it('should return a user', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/users/${user.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200);
+      const data = await res.body;
+      expect(data).not.toBeNull();
+      expect(data.id).toEqual(user.id);
+      expect(data.username).toEqual(user.username);
+    });
+  });
+
+  describe('Update', () => {
+    it('should throw error 400 when param input wrong', async () => {
+      await request(app.getHttpServer())
+        .patch('/users/a')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(400);
+    });
+
+    it('should throw error 404 when user not found', async () => {
+      await request(app.getHttpServer())
+        .patch('/users/1000')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ role: {} })
+        .expect(404);
+    });
+
+    it('should return updated user', async () => {
+      const updateUser = {
+        username: 'anotheruser',
+        name: 'Updated user name',
+        email: 'test@email.com',
+        password: 'aaaaa',
+        image: 'imagepath',
+        blocked: true,
+        roleId: 1,
+      };
+      const res = await request(app.getHttpServer())
+        .patch('/users/1')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(updateUser)
+        .expect(200);
+      const { username, password, email, name, image, blocked, roleId } =
+        await res.body;
+      console.log(JSON.stringify(await res.body, null, 4));
+      expect(username).toEqual(updateUser.username);
+      expect(email).toEqual(updateUser.email);
+      expect(name).toEqual(updateUser.name);
+      expect(image).toEqual(updateUser.image);
+      expect(blocked).toEqual(updateUser.blocked);
+      expect(roleId).toEqual(updateUser.roleId);
+      expect(await compareValue(updateUser.password, password)).toEqual(true);
     });
   });
 });
