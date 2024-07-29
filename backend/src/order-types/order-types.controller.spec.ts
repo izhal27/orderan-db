e5897a7dd01251c0f7aeeaa45cc0e91ce2c7de71
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-30 06:28:38 +0800

    add order type controler test

--- backend/src/order-types/order-types.controller.spec.ts
+++ backend/src/order-types/order-types.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+
 import { OrderTypesController } from './order-types.controller';
 import { OrderTypesService } from './order-types.service';
+import { CreateOrderTypeDto } from './dto/create-order-type.dto';
+import { UpdateOrderTypeDto } from './dto/update-order-type.dto';
 
-describe('OrderTypeController', () => {
+describe('OrderTypesController', () => {
   let controller: OrderTypesController;
+  let serviceMock: DeepMockProxy<OrderTypesService>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    serviceMock = mockDeep<OrderTypesService>();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderTypesController],
-      providers: [OrderTypesService],
+      providers: [
+        {
+          provide: OrderTypesService,
+          useValue: serviceMock,
+        },],
     }).compile();
 
     controller = module.get<OrderTypesController>(OrderTypesController);
@@ -16,5 +27,60 @@ describe('OrderTypeController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findMany', () => {
+    it('should be defined', () => {
+      expect(serviceMock.findMany).toBeDefined();
+    });
+
+    it('should call RolesService.findMany', () => {
+      controller.findAll();
+      expect(serviceMock.findMany).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should be defined', () => {
+      expect(serviceMock.findUnique).toBeDefined();
+    });
+
+    it('should call RolesService.findUnique', () => {
+      controller.findOne(1);
+      expect(serviceMock.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(serviceMock.create).toBeDefined();
+    });
+
+    it('should call RolesService.create', () => {
+      controller.create({} as CreateOrderTypeDto);
+      expect(serviceMock.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should be defined', () => {
+      expect(serviceMock.update).toBeDefined();
+    });
+
+    it('should call RolesService.update', () => {
+      controller.update(1, {} as UpdateOrderTypeDto);
+      expect(serviceMock.update).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('delete', () => {
+    it('should be defined', () => {
+      expect(serviceMock.delete).toBeDefined();
+    });
+
+    it('should call UsersService.delete', () => {
+      controller.delete(1);
+      expect(serviceMock.delete).toHaveBeenCalledTimes(1);
+    });
   });
 });
