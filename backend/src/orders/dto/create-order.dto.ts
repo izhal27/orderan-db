80e7554b0c7f1892cb511897396d8b9cf1fb06da
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-30 16:29:32 +0800

    add order dto

--- backend/src/orders/dto/create-order.dto.ts
+++ backend/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,43 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  number: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty()
+  date: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  customer: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
+  @ApiProperty({ required: false })
+  description: string | null;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  userId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  updatedBy: number | null;
+}
