1e611b0ca1a5301f9336ce85c7e4e1fe80cd9d2b
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-30 16:29:59 +0800

    add import prisma module

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
