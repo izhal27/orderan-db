41e7a9eb756ddf7e54da21059b5d97aa0cc5e0c2
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-30 16:29:13 +0800

    add order entity

--- backend/src/orders/entities/order.entity.ts
+++ backend/src/orders/entities/order.entity.ts
@@ -1,1 +1,31 @@
-export class Order {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Order } from "@prisma/client";
+
+export class OrderEntity implements Order {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  number: string;
+
+  @ApiProperty()
+  date: Date;
+
+  @ApiProperty()
+  customer: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  userId: number;
+
+  @ApiProperty({ required: false, nullable: true })
+  updatedBy: number | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
