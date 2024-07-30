9ed946eae3d22ee1b9df842ffcd774a16bcffe5e
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-31 06:50:04 +0800

    delete userId from user

---
+++ backend/prisma/migrations/20240730224924_delete_user_id_from_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `orderId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "orderId";
