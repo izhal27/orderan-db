0333468f356754a736e557e5c0c7f446420c5811
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-01 07:50:37 +0800

    set global prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
   });
+  const configService = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,20 +21,19 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  const configService = app.get<ConfigService>(ConfigService);
-
-  const config = new DocumentBuilder()
-    .setTitle('Orderan Dunia Baliho')
-    .setDescription('The Orderan Dunia Baliho API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  app.setGlobalPrefix('api');
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Orderan Dunia Baliho')
+      .setDescription('The Orderan Dunia Baliho API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-
   await app.listen(configService.get('PORT') || 3002);
 }
 bootstrap();
