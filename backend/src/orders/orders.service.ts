2feed24dd08ad28738c20954090076f554d3c891
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-31 08:33:37 +0800

    add create order

--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -2,37 +2,42 @@ import { Injectable, NotFoundException, NotImplementedException } from '@nestjs/
 import { Order, Prisma } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
 
-import { OrderDetailsService } from '../order-details/order-details.service';
-import { OrderEntity } from './entities/order.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Injectable()
 export class OrdersService {
-  constructor
-    (private readonly prismaService: PrismaService,
-      private readonly orderDetailsService: OrderDetailsService
-    ) { }
+  constructor(private readonly prismaService: PrismaService) { }
 
-  async create(createOrderDto: CreateOrderDto, userId: number): Promise<Order> {
+  async create(createOrderDto: CreateOrderDto, userId: number): Promise<Order[]> {
+    const { number, date, customer, description, orderDetails } = createOrderDto;
     let result;
     await this.prismaService.$transaction(async (prisma) => {
-      const { orderDetails, ...orderObj } = createOrderDto;
-      const order = await prisma.order.create({ data: orderObj }) as OrderEntity;
-      const resOrderDetails = await this.orderDetailsService.createManyAndReturn(order.id, orderDetails);
-      order.orderDetails = resOrderDetails;
-      return order;
+      return prisma.order.create({
+        data: {
+          number,
+          date,
+          customer,
+          description,
+          userId,
+          orderDetails: {
+            create: orderDetails
+          }
+        }, include: {
+          orderDetails: true
+        }
+      })
     }).then(data => { result = data })
       .catch(error => console.log(JSON.stringify(error, null, 4)));
     return result;
   }
 
   findMany(): Promise<Order[] | null> {
-    return this.prismaService.order.findMany();
+    return this.prismaService.order.findMany({ include: { orderDetails: true } });
   }
 
   async findUnique(where: Prisma.OrderWhereUniqueInput): Promise<Order | null> {
-    const order = await this.prismaService.order.findUnique({ where });
+    const order = await this.prismaService.order.findUnique({ where, include: { orderDetails: true } });
     if (!order) {
       throw new NotFoundException('Order not found');
     }
