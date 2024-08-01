255d4aad9e09283bd1abd42cd5e0e1fa23d8cbbd
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-02 07:43:31 +0800

    add customers controller

--- backend/src/customers/customers.controller.spec.ts
+++ backend/src/customers/customers.controller.spec.ts
@@ -1,20 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CustomersController } from './customers.controller';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+
 import { CustomersService } from './customers.service';
+import { CustomersController } from './customers.controller';
+import { CreateCustomerDto, UpdateCustomerDto } from './dto';
 
 describe('CustomersController', () => {
   let controller: CustomersController;
+  let serviceMock: DeepMockProxy<CustomersService>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    serviceMock = mockDeep<CustomersService>();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
-      providers: [CustomersService],
+      providers: [
+        {
+          provide: CustomersService,
+          useValue: serviceMock,
+        },],
     }).compile();
 
     controller = module.get<CustomersController>(CustomersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findMany', () => {
+    it('should be defined', () => {
+      expect(serviceMock.findMany).toBeDefined();
+    });
+
+    it('should call RolesService.findMany', () => {
+      controller.findAll();
+      expect(serviceMock.findMany).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should be defined', () => {
+      expect(serviceMock.findUnique).toBeDefined();
+    });
+
+    it('should call RolesService.findUnique', () => {
+      controller.findOne('1');
+      expect(serviceMock.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(serviceMock.create).toBeDefined();
+    });
+
+    it('should call RolesService.create', () => {
+      controller.create({} as CreateCustomerDto, 1);
+      expect(serviceMock.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should be defined', () => {
+      expect(serviceMock.update).toBeDefined();
+    });
+
+    it('should call RolesService.update', () => {
+      controller.update('1', {} as UpdateCustomerDto);
+      expect(serviceMock.update).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('delete', () => {
+    it('should be defined', () => {
+      expect(serviceMock.delete).toBeDefined();
+    });
+
+    it('should call UsersService.delete', () => {
+      controller.delete('1');
+      expect(serviceMock.delete).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- backend/src/customers/customers.controller.ts
+++ backend/src/customers/customers.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+import { CustomerEntity } from './entities/customer.entity';
 import { CustomersService } from './customers.service';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { CreateCustomerDto } from './dto/create-customer.dto';
-import { CustomerEntity } from './entities/customer.entity';
+import { GetCurrentUserId } from '../common/decorators';
 
 @Controller('customers')
 @ApiTags('customers')
@@ -14,31 +15,31 @@ export class CustomersController {
 
   @Post()
   @ApiCreatedResponse({ type: CustomerEntity })
-  create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customersService.create(createCustomerDto);
+  create(@Body() createCustomerDto: CreateCustomerDto, @GetCurrentUserId() userId: number) {
+    return this.customersService.create(createCustomerDto, userId);
   }
 
   @Get()
   @ApiOkResponse({ type: CustomerEntity, isArray: true })
   findAll() {
-    return this.customersService.findAll();
+    return this.customersService.findMany();
   }
 
   @Get(':id')
   @ApiOkResponse({ type: CustomerEntity })
   findOne(@Param('id') id: string) {
-    return this.customersService.findOne(+id);
+    return this.customersService.findUnique({ id });
   }
 
   @Patch(':id')
   @ApiOkResponse({ type: CustomerEntity })
   update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customersService.update(+id, updateCustomerDto);
+    return this.customersService.update({ where: { id }, data: updateCustomerDto });
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: CustomerEntity })
-  remove(@Param('id') id: string) {
-    return this.customersService.remove(+id);
+  delete(@Param('id') id: string) {
+    return this.customersService.delete({ id });
   }
 }
