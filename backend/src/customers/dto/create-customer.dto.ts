f4cefae591f840d70f760a6d04f6eec5f6b0b904
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-02 05:58:53 +0800

    add decorators

--- backend/src/customers/dto/create-customer.dto.ts
+++ backend/src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,32 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Exclude } from "class-transformer";
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  address: string | null;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(50)
+  contact: string | null;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(50)
+  email: string | null;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(300)
+  description: string | null;
+
+  @Exclude()
+  createdById: number | null;
+}
--- backend/src/customers/entities/customer.entity.ts
+++ backend/src/customers/entities/customer.entity.ts
@@ -1,1 +1,31 @@
-export class Customer {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Customer } from "@prisma/client";
+
+export class CustomerEntity implements Customer {
+  @ApiProperty({ required: false })
+  id: string;
+
+  @ApiProperty({ example: "John Doe", description: "Customer name" })
+  name: string;
+
+  @ApiProperty({ required: false, example: "27th Street", description: "Customer address" })
+  address: string | null;
+
+  @ApiProperty({ required: false })
+  contact: string | null;
+
+  @ApiProperty({ required: false })
+  email: string | null;
+
+  @ApiProperty({ required: false })
+  description: string | null;
+
+  @ApiProperty({ required: false })
+  createdById: number | null;
+
+  @ApiProperty({ required: false })
+  createdAt: Date;
+
+  @ApiProperty({ required: false })
+  updatedAt: Date;
+}
