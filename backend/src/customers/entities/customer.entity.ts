7b13d4b1a3d8aca8b8fe437bbf761a4e8411bd4a
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-02 07:12:20 +0800

    add example to decorators customer

--- backend/src/customers/entities/customer.entity.ts
+++ backend/src/customers/entities/customer.entity.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Customer } from "@prisma/client";
 
 export class CustomerEntity implements Customer {
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: '925dd154-3e78-4fba-b3c9-ace2504f31e9' })
   id: string;
 
   @ApiProperty({ example: "John Doe", description: "Customer name" })
@@ -11,21 +11,21 @@ export class CustomerEntity implements Customer {
   @ApiProperty({ required: false, example: "27th Street", description: "Customer address" })
   address: string | null;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: "+62 12345", description: "Customer contact" })
   contact: string | null;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: "customer@email.com", description: "Customer email" })
   email: string | null;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: "Donec elit libero sodales nec", description: "Customer description" })
   description: string | null;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: 1, description: "Current user id" })
   createdById: number | null;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: new Date().toISOString() })
   createdAt: Date;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, example: new Date().toISOString() })
   updatedAt: Date;
 }
