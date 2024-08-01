9776a1478bfbeee71a6a890dbe1a1b4a9af9ab95
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-02 07:32:12 +0800

    add import prisma module

--- backend/src/customers/customers.module.ts
+++ backend/src/customers/customers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'nestjs-prisma';
+
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule { }
