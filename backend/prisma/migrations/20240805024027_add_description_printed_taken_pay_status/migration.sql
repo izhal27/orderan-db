a83cd04eb86562054e56e0fc166228bd291ecf9f
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-05 10:41:06 +0800

    add description printed, taken, pay status

---
+++ backend/prisma/migrations/20240805024027_add_description_printed_taken_pay_status/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "PayStatus" ADD COLUMN     "description" TEXT;
+
+-- AlterTable
+ALTER TABLE "PrintedStatus" ADD COLUMN     "description" TEXT;
+
+-- AlterTable
+ALTER TABLE "TakenStatus" ADD COLUMN     "description" TEXT;
