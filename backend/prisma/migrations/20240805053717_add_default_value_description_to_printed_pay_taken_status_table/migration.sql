50ea0764c94db183e0a71dbe379e8effab85d9e9
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-05 13:55:38 +0800

    add default value description to printed pay takenstatus table

---
+++ backend/prisma/migrations/20240805053717_add_default_value_description_to_printed_pay_taken_status_table/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "PayStatus" ALTER COLUMN "description" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "PrintedStatus" ALTER COLUMN "description" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "TakenStatus" ALTER COLUMN "description" SET DEFAULT '';
