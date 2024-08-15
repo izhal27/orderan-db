58c0f68f100a6d7451d88bbdd4000f246c0c200f
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-16 05:24:46 +0800

    remove image from home dashboard

--- frontend/app/(dashboard)/content.tsx
+++ frontend/app/(dashboard)/content.tsx
@@ -5,7 +5,6 @@ import React from "react";
 export const HomePageContent: NextPage = function () {
   return (
     <div className="p-6 mx-auto">
-      <Image src={'/db-image.png'} width={300} height={300} alt="Logo Dunia Baliho" />
     </div>
   );
 };
