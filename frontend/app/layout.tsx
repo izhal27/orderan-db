c18e308965988dbb8d37ee28ac49db12c5aa36a0
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-16 13:36:58 +0800

    add metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,18 @@ import { flowbiteTheme } from "./theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: 'Orderan Dunia Baliho',
+  description: 'Sistem orderan dunia baliho',
+  author: 'Risal Walangadi',
+  authors: [
+    { name: 'Risal Walangadi', url: 'https://github.com/izhal27' }
+  ],
+  icons: {
+    icon: '/favicon.png'
+  }
+}
+
 const RootLayout: FC<PropsWithChildren> = function ({ children }) {
   return (
     <html lang="en">
