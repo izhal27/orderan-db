1f456c41018d50b0cff1ebcdcbd627df81371ffb
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-20 17:49:46 +0800

    add customer and user type

--- frontend/types/constant.ts
+++ frontend/types/constant.ts
@@ -1,5 +1,36 @@
 export interface OrderType {
-  id: string;
+  id: number;
   name: string;
   description: string;
+}
+
+export interface Customer {
+  id: string;
+  name: string;
+  address: string;
+  contact: string;
+  email: string;
+  description: string;
+  createdById: number;
+  user: {
+    id: number;
+    username: string;
+    email: string;
+    name: string;
+    image: string;
+  }
+}
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  name: string;
+  image: string;
+  blocked: boolean;
+  roleId: number;
+  role: {
+    id: number,
+    name: string,
+  }
 }
