9b39159483f809e16cee92a6138802097f99972b
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-21 11:12:35 +0800

    add users page

---
+++ frontend/app/(dashboard)/data/users/page.tsx
@@ -0,0 +1,22 @@
+import AddButton from "@/components/buttons/AddButton";
+
+export default function UsersPage() {
+  return (
+    <main className="flex flex-col gap-4 p-4">
+      <div>
+        <h1 className="text-2xl font-bold text-gray-500 dark:text-gray-400">
+          Users
+        </h1>
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Menampilkan daftar pengguna applikasi
+        </p>
+      </div>
+      <div className="flex justify-end">
+        <div className="max-w-32">
+          <AddButton />
+        </div>
+      </div>
+      <h1>Table</h1>
+    </main>
+  );
+}
