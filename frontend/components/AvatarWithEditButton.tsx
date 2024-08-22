8550e6d1a5f4167a6767c19c034af8c529ddb924
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-22 10:58:26 +0800

    fix toggle select user avatar

--- frontend/components/AvatarWithEditButton.tsx
+++ frontend/components/AvatarWithEditButton.tsx
@@ -1,22 +1,88 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useRef, useState } from 'react';
 import { Avatar } from 'flowbite-react';
-import { FiEdit2 } from 'react-icons/fi';
-import UserAvatar from './UserAvatar';
+import { twMerge } from "tailwind-merge";
+import { HiPencil, HiX } from 'react-icons/hi';
 
 interface props {
   userImage: string | undefined;
 }
 
 const AvatarWithEditButton = ({ userImage }: props) => {
+  const [img, setImg] = useState<string | undefined>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    userImage && setImg(userImage);
+  }, [userImage]);
+
+  const onClickHandler = () => {
+    if (!img) {
+      inputRef?.current?.click();
+    } else {
+      inputRef!.current!.value = '';
+      setImg(undefined);
+    }
+  }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImg(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  let avatarImg = <Avatar
+    bordered
+    rounded
+    size={'xl'}
+    alt='User Avatar'
+  />;
+
+  if (img) {
+    avatarImg = <Avatar
+      bordered
+      rounded
+      size={'xl'}
+      img={(props) => {
+        props.className = props.className + ' object-cover';
+        return (<img
+          alt="User Avatar"
+          width={40}
+          height={40}
+          src={img}
+          {...props}
+        />);
+      }}
+    />
+  }
+
   return (
     <div className="relative w-36 h-36">
-      <UserAvatar userImage={userImage} width={144} height={144} size='xl' bordered />
+      {avatarImg}
       <button
         type="button"
-        className="absolute top-0 right-2 p-2 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        onClick={onClickHandler}
+        className={
+          twMerge("absolute top-0 right-2 p-2 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none",
+            img && 'bg-red-600 hover:bg-red-700 focus:ring-2 focus:ring-red-500'
+          )}
       >
-        <FiEdit2 size={16} />
+        {img ? <HiX size={16} /> : <HiPencil size={16} />}
       </button>
+      <input
+        ref={inputRef}
+        type="file"
+        id="avatarInput"
+        accept="image/*"
+        onChange={handleImageChange}
+        className="hidden"
+      />
     </div>
   );
 };
