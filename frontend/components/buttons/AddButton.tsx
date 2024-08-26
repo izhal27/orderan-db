3f7404b8d694dd11bf4edbcc7db5208f35407bc0
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-27 06:12:14 +0800

    add params text

--- frontend/components/buttons/AddButton.tsx
+++ frontend/components/buttons/AddButton.tsx
@@ -5,13 +5,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoIosAdd } from "react-icons/io";
 
-export default function AddButton() {
+interface props {
+  text?: string;
+}
+
+export default function AddButton({ text = 'Tambah' }: props) {
   const currentPath = usePathname();
 
   return (
     <Button size={"sm"} color={"blue"} as={Link} href={`${currentPath}/add`}>
       <IoIosAdd className="mr-2 size-5" />
-      Tambah
+      {text}
     </Button>
   );
 }
