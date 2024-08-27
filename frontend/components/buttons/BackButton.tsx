1d189da7bdf1b41be6c7d78ebc1db7bb88576b21
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-28 07:00:23 +0800

    change to inline-flex

--- frontend/components/buttons/BackButton.tsx
+++ frontend/components/buttons/BackButton.tsx
@@ -7,7 +7,7 @@ export default function BackButton() {
   return (
     <div>
       <a
-        className="flex cursor-pointer text-sm font-light text-gray-900 dark:text-white"
+        className="inline-flex cursor-pointer text-sm font-light text-gray-900 dark:text-white"
         onClick={() => router.back()}
       >
         <HiArrowLeft className="mr-2 size-5" />
