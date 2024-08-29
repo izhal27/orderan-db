b0eb463f82462752599f8a13e77dd664bbf29295
Risal Walangadi <risal.walangadi@gmail.com>
2024-08-29 18:18:35 +0800

    add order list dynamic edit page

---
+++ frontend/app/(dashboard)/orders/list/[id]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Order } from "@/constants";
+import OrderAddEdit from "../_components/AddEdit";
+
+export default function EditPage({ params }: { params: { id: string } }) {
+  const { data: session } = useSession();
+  const [order, setCustomer] = useState<Order | undefined>(undefined);
+
+  useEffect(() => {
+    if (session) {
+      const fetchData = async () => {
+        const res = await fetch(
+          `http://localhost:3002/api/orders/${params.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${session?.accessToken}`,
+              "Content-Type": "application/json",
+            },
+            cache: "no-store",
+          },
+        );
+        setCustomer(await res.json());
+      };
+      fetchData();
+    }
+  }, [session]);
+
+  return <OrderAddEdit order={order} />;
+}
