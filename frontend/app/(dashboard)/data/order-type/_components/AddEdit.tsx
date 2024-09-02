1e4d5bc9de620bff24b2786da471d4780edb7fb6
Risal Walangadi <risal.walangadi@gmail.com>
2024-09-02 19:48:30 +0800

    refactor

--- frontend/app/(dashboard)/data/order-type/_components/AddEdit.tsx
+++ frontend/app/(dashboard)/data/order-type/_components/AddEdit.tsx
@@ -3,14 +3,13 @@
 import BackButton from "@/components/buttons/BackButton";
 import type { OrderTypeFormData } from "@/constants/formTypes";
 import type { OrderType } from "@/constants/interfaces";
-import { useLoading } from "@/context/LoadingContext";
 import { showToast } from "@/helpers/toast";
 import { orderTypeSchema } from "@/schemas/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface props {
@@ -26,11 +25,10 @@ export default function OrderTypeAddEdit({ orderType }: props) {
     handleSubmit,
     setValue,
     setFocus,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<OrderTypeFormData>({
     resolver: zodResolver(orderTypeSchema),
   });
-  const { isLoading, setLoading } = useLoading();
 
   useEffect(() => {
     setFocus("name");
@@ -41,7 +39,6 @@ export default function OrderTypeAddEdit({ orderType }: props) {
   }, [orderType]);
 
   const onSubmit = async (data: OrderTypeFormData) => {
-    setLoading(true);
     return !isEditMode ? addHandler(data) : editHandler(orderType!.id, data);
   };
 
@@ -75,14 +72,12 @@ export default function OrderTypeAddEdit({ orderType }: props) {
         "success",
         `Jenis Pesanan "${orderType.name}" berhasil ${isEditMode ? "disimpan" : "ditambahkan"}.`,
       );
-      setLoading(false);
       router.back();
     } else if (res.status == 409) {
       showToast("error", "Nama sudah digunakan, coba dengan nama yang lain.");
     } else {
       showToast("error", "Terjadi kesalahan, coba lagi nanti.");
     }
-    setLoading(false);
   }
 
   return (
@@ -117,9 +112,9 @@ export default function OrderTypeAddEdit({ orderType }: props) {
           </div>
         </div>
         <div className="flex gap-2">
-          <Button color={"blue"} type="submit" disabled={isLoading}>
-            {isLoading && <Spinner size={"sm"} />}
-            <span className={isLoading ? "pl-3" : ""}>
+          <Button color={"blue"} type="submit" disabled={isSubmitting}>
+            {isSubmitting && <Spinner size={"sm"} />}
+            <span className={isSubmitting ? "pl-3" : ""}>
               {isEditMode ? "Simpan" : "Tambah"}
             </span>
           </Button>
