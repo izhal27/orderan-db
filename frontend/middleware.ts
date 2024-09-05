743feb491711b732be31753c285e986c2b4706c8
Risal Walangadi <risal.walangadi@gmail.com>
2024-09-05 11:53:51 +0800

    fix redirect path

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,8 +5,8 @@ import { auth } from "@/auth";
 export default auth(async (req) => {
   const session = await auth();
   const role = session?.user.role;
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
+  if (!req.auth && req.nextUrl.pathname !== "/auth/signin") {
+    const newUrl = new URL("/auth/signin", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
   if (
