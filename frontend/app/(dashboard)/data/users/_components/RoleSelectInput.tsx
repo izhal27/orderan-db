21c084ddb6ecc673f72abfbf65ba1d06c6aeabc3
Risal Walangadi <risal.walangadi@gmail.com>
2024-09-14 18:50:30 +0800

    fix get roles

--- frontend/app/(dashboard)/data/users/_components/RoleSelectInput.tsx
+++ frontend/app/(dashboard)/data/users/_components/RoleSelectInput.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { COMMON_ERROR_MESSAGE, showToast } from "@/helpers";
+import { useApiClient } from "@/lib/apiClient";
 import { Label, Select } from "flowbite-react";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 interface Role {
   id: number;
@@ -21,32 +23,35 @@ export function RoleSelectInput({
   const { data: session } = useSession();
   const [roles, setRoles] = useState<Role[]>([]);
   const [userRoleId, setUserRoleId] = useState<number | null>(null);
+  const { request } = useApiClient();
+
+  const fetchRoles = useCallback(async () => {
+    if (selectedUserRoleId) {
+      setUserRoleId(selectedUserRoleId);
+    }
+    try {
+      const roles = await request("/roles");
+      setRoles(roles);
+    } catch (error) {
+      showToast("error", COMMON_ERROR_MESSAGE);
+    }
+  }, [request, selectedUserRoleId, setUserRoleId]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (selectedUserRoleId) {
-        setUserRoleId(selectedUserRoleId);
-      }
-      const res = await fetch("http://localhost:3002/api/roles", {
-        headers: {
-          Authorization: `Bearer ${session?.accessToken}`,
-          "Content-Type": "application/json",
-        },
-        cache: "force-cache",
-      });
-      if (res.ok) {
-        const result = await res.json();
-        setRoles(result);
-      }
-    };
-    if (session) {
-      fetchData();
+    if (session?.accessToken) {
+      fetchRoles();
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session, selectedUserRoleId]);
 
-  const changeHandler = (event: React.FormEvent<HTMLSelectElement>) => {
-    onSelectHandler(+event.currentTarget.value);
-  };
+  const changeHandler = useCallback(
+    (event: React.FormEvent<HTMLSelectElement>) => {
+      onSelectHandler(+event.currentTarget.value);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
   return (
     <div>
