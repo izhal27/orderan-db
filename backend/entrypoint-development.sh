f2797aeffa5883b0d8de6ffe48e6823ef807275c
Risal Walangadi <risal.walangadi@gmail.com>
2024-09-15 08:06:51 +0800

    refactor environment variables

--- backend/entrypoint-development.sh
+++ backend/entrypoint-development.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # entrypoint.sh
 
-# Run Prisma migrations
-yarn prisma migrate deploy
-
-# Run Prisma seed
-yarn prisma db seed
+# Run Prisma migrate reset
+yarn prisma migrate reset --force
 
 # Run NestJS application
 yarn start:dev
