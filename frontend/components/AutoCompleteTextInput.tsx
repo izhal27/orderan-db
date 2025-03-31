92006a50a7220fe4d07f4c26c976d39db157beb9
Risal Walangadi <risal.walangadi@gmail.com>
2025-03-31 16:48:56 +0800

    refactor

--- frontend/components/AutoCompleteTextInput.tsx
+++ frontend/components/AutoCompleteTextInput.tsx
@@ -132,10 +132,11 @@ export default function AutoCompleteTextInput<T>({
           {items.map((item, index) => (
             <li
               key={getKeyValue(item)}
-              className={`cursor-pointer p-2 ${index === activeIndex
-                ? "bg-blue-500 text-white"
-                : "bg-white text-black"
-                }`}
+              className={`cursor-pointer p-2 ${
+                index === activeIndex
+                  ? "bg-blue-500 text-white"
+                  : "bg-white text-black"
+              }`}
               onClick={() => handleSuggestionClick(item)}
               onMouseEnter={() => setActiveIndex(index)}
               onKeyDown={(e) => {
