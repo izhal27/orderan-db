e6aeb2caad685745a42fb9a94ac0f44b746376be
Risal Walangadi <risal.walangadi@gmail.com>
2026-03-25 14:47:18 +0800

    fix: switch to debian images and stabilize user UI (#19)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,11 @@
 npx prisma migrate deploy
 
 # Run Prisma seed
-node dist/prisma/seed.js
+if [ -f dist/prisma/seed.js ]; then
+  node dist/prisma/seed.js
+else
+  npx prisma db seed
+fi
 
 # Run NestJS application
 exec pm2-runtime start dist/src/main.js --name "backend"
